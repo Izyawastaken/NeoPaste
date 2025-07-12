7080948d65cd457e89b0ea47d2fd450f75407fa0
Izzy <agastyawastaken@gmail.com>
2025-07-12 15:02:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-const SUPABASE_URL = window.SUPABASE_PROJECT_LINK;
-const SUPABASE_KEY = window.SUPABASE_ANON_KEY;
+// config.js
+const SUPABASE_URL = 'https://pzswzyxixfkeowjgrlvw.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6c3d6eXhpeGZrZW93amdybHZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MjE0MDcsImV4cCI6MjA2NzI5NzQwN30.i4JQPHdM9ostv9iWLk57NMGMQ7zaX5woZOIKCbQVtHM';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
